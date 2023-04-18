38db84c3e6d76bdac86e931d50d8955984be76e1
pramudithaSJ <IT20620202@my.sliit.lk>
2023-04-18 23:48:42 +0530

    Update Session.js

--- frontend/src/userView/Session/Session.js
+++ frontend/src/userView/Session/Session.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Session() {
   return (
-    <div>
-      
+    <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
+      <div className="bg-black w-full h-5">
+        <img src="" alt="image" />
+      </div>
+
+      <div className="text-2xl font-semibold  text-center text-gray-500 truncate">
+        Total users
+      </div>
+      <div className="mt-1 text-sm font-medium justify text-gray-900">
+        12,00
+      </div>
     </div>
-  )
+  );
 }
 
-export default Session
+export default Session;
