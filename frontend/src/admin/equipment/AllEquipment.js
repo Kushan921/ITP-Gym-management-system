ddf09964e46b792a0098bd05fbfa625102455b81
pramudithaSJ <IT20620202@my.sliit.lk>
2023-04-18 20:58:23 +0530

    Update AllEquipment.js

--- frontend/src/admin/equipment/AllEquipment.js
+++ frontend/src/admin/equipment/AllEquipment.js
@@ -12,7 +12,7 @@ const customStyles = {
   content: {
     top: "50%",
     left: "50%",
-    right: "auto",
+    right: "20%",
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
