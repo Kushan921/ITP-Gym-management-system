277d103b4cdc62fb4481518368dd3b9251446660
Kushan <kushanmigara@gmail.com>
2023-04-19 01:39:31 +0530

    suppliment user view

--- frontend/src/userView/Suppliments/suppliments.js
+++ frontend/src/userView/Suppliments/suppliments.js
@@ -1,38 +1,39 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Suppliments() {
+  const [store, setStore] = useState([]);
 
-    const [store,setStore] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:8020/item/')
+      .then((response) => setStore(response.data))
+      .catch((error) => console.error(error));
+  }, []);
 
-    useEffect(() => {
-        axios.get("http://localhost:8020/store/")
-          .then((response) => {setStore(response.data)})
-          .catch((error) => console.error(error))
-      },[]);
+  const rows = [];
+  for (let i = 0; i < store.length; i += 3) {
+    rows.push(store.slice(i, i + 3));
+  }
 
   return (
     <div>
-        {store.map((store) => (
-
-            <div class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-                <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                    {store.item_name}
-                </h5>
-                <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                    {store.description}
-                </p>
-                <button
-                    type="button"
-                    class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                    data-te-ripple-init
-                    data-te-ripple-color="light">
-                    Button
-                </button>
+        <p style={{fontSize:"40px",textAlign:"center"}}>Suppliments</p>
+      {rows.map((row, rowIndex) => (
+        <div key={rowIndex} className="flex flex-col md:flex-row md:justify-between ">
+          {row.map((item, itemIndex) => (
+            <div key={itemIndex} className="w-full md:w-1/3 px-4 py-5 bg-white rounded-lg shadow md:mb-0 mb-4 m-5" >
+              <div className="bg-black w-full h-5">
+                <img src={item.image} alt="image" style={{width:"150px",height:"100px"}}/>
+              </div>0o 
+              <div className="text-2xl font-semibold text-center text-gray-500 truncate">{item.item_name}</div>
+              <div className="mt-1 text-sm font-medium justify text-gray-900">{item.price}</div>
             </div>
-        ))} 
+          ))}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Suppliments
+export default Suppliments;
