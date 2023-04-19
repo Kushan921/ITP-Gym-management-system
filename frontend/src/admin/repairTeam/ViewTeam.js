f74f7ae5385e60ef140e6c604e15f7a04ca07ca5
pramudithaSJ <IT20620202@my.sliit.lk>
2023-04-19 20:26:17 +0530

    Update ViewTeam.js

--- frontend/src/admin/repairTeam/ViewTeam.js
+++ frontend/src/admin/repairTeam/ViewTeam.js
@@ -392,7 +392,7 @@ export default function AllTeam() {
                     className="bg-green-800 w-1/2 text-white py-3 hover:bg-green-500"
                     type="submit"
                   >
-                    Update
+                    Create
                   </button>
                 </div>
               </Form>
