775cfbe36ec400c47b1ba3b0b200d3a790d57794
Kushan <kushanmigara@gmail.com>
2023-04-19 09:10:55 +0530

    add repair team add form

--- frontend/src/admin/repairTeam/ViewTeam.js
+++ frontend/src/admin/repairTeam/ViewTeam.js
@@ -134,6 +134,31 @@ export default function AllTeam() {
       <div className="w-full bg-gray-100 py-10 text-center">
         <h1 className="text-2xl">Repair Team Details</h1>
       </div>
+      <div className="w-full flex flex-row-reverse px-10 mt-10">
+        <button
+          type="button"
+          onClick={() => {
+            setIsNewOpen(true);
+          }}
+          class="  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          {" "}
+          Add New
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </div>
 
       <div className=" px-10 mt-10 ">
         <div class=" shadow-md sm:rounded-lg ">
@@ -396,6 +421,7 @@ export default function AllTeam() {
             initialValues={{
               team_name: team_name,
               specialization: specialization,
+              gender: gender,
               description: description,
               NoOfPeople: NoOfPeople,
               hourPrice: hourPrice,
